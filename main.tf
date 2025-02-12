9bb995ec1aa69f729317794f749ca3cd85a51d7f
Say-re <91860677+Say-re@users.noreply.github.com>
2025-02-11 22:00:52 -0800

    Add creation of dynamoDB table and S3 bucket for storing data

--- main.tf
+++ main.tf
@@ -13,3 +13,48 @@ provider "aws" {
     region = "us-west-2"
     profile = "default"
 }
+
+# Create S3 Bucket for Storing User Provided Data (CSV)
+resource "aws_s3_bucket" "energy-monitoring-pdx" {
+    bucket = "energy-monitoring-pdx"
+
+    tags = {
+        Name = "Household Energy Monitoring Data"
+        Environment = "production"
+    }
+}
+
+# Create Dynamo DB Table
+resource "aws_dynamodb_table" "usr_energy_consumption" {
+    name = "energyConsumption"
+    billing_mode = "PAY_PER_REQUEST"
+
+    # Primary Key
+    attribute {
+        name = "userId"
+        type = "S"
+    }
+
+    attribute {
+        name = "date"
+        type = "S"
+    }
+
+    hash_key = "userId"
+    sort_key = "date"
+
+    # GSI Setup
+    global_secondary_index {
+        name = "dateIndex"
+        hash_key = "date"
+        range_key = "userId"
+        projection_type = "ALL"
+        write_capacity = 5
+        read_capacity = 5
+    }
+
+    tags = {
+        Name = "User Energy Consumption Table"
+        Environment = "Production"
+    }
+}
